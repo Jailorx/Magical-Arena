7624fa0cb1497d3c6bb97daf6f7b1556c453e912
Shubham Bisht <bisht.shubham.emp@gmail.com>
2024-02-23 19:43:32 +0530

    Adding class to define global constants like strength and attack

---
+++ src/game/constants/AppConstants.java
@@ -0,0 +1,9 @@
+package src.game.constants;
+
+public class AppConstants {
+    public static final int MAX_HEALTH=100;
+    public static final int MAX_STRENGTH=10;
+    public static final int MAX_ATTACK=10;
+
+    
+}
